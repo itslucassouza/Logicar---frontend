e64827a08e7996dd401ebaa98a1e8fb93cb81c3f
itslucassouza <lucasvini193@hotmail.com>
2023-10-23 19:54:24 -0300

    adjust text

--- src/view/pages/Login/index.tsx
+++ src/view/pages/Login/index.tsx
@@ -60,7 +60,7 @@ export function Login() {
                 </div>
                 <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
                   <div className="text-gray-500 text-center mb-3 font-bold">
-                    <small>faça login com credenciais</small>
+                    <small>faça login com suas credenciais</small>
                   </div>
                   <form>
                     <div className="relative w-full mb-3">
